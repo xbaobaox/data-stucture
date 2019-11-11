e5a4fabe270feb98d22ddf97c192611ae7152f09
xbaobaox <xbaobaox@sina.com>
2019-11-11 22:11:50 +0800

    11.11 22:11

--- abcde.js
+++ abcde.js
@@ -1,20 +1,31 @@
-var str = 'abcde';
-function reverse(st) {
-  var arr=[]
-  for (var i = 0; i < st.length; i++){
-    arr.push(st[i]);
-  }
-  var revArr=[]
-  for (var j = arr.length-1; j >= 0; j--){
-    revArr.push(arr[j]);
-  }
-  return revArr.join('');
-}
-var result = reverse(str);
-console.log(result);
+// var str = 'abcde';
+// function reverse(st) {
+//   var arr=[]
+//   for (var i = 0; i < st.length; i++){
+//     arr.push(st[i]);
+//   }
+//   var revArr=[]
+//   for (var j = arr.length-1; j >= 0; j--){
+//     revArr.push(arr[j]);
+//   }
+//   return revArr.join('');
+// }
+// var result = reverse(str);
+// console.log(result);
 
 
 
 
 // var fuck = str.split('').reverse().join('');
 // console.log(fuck);
+
+var str = 'abcde';
+function reverse(st) {
+  var arr = []
+  for (var i = 0; i < st.length; i++) {
+    arr.unshift(st[i]);
+  }
+  return arr.join('');
+}
+var result = reverse(str);
+console.log(result);
